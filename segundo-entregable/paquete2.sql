a88dcc7f5dec110fe078f36405bea3f68b0588ae
Juan Carlos Cortés Muñoz <56033673+juacormun@users.noreply.github.com>
2019-04-14 15:29:23 +0000

    paquete2.sql añadido

---
+++ segundo-entregable/paquete2.sql
@@ -0,0 +1,46 @@
+-- REGISTRO DE VOLUNTARIOS
+
+EXEC Registrar_Voluntario('54786321L', 'Pedro', 'Castaño Moreno', '23/11/1990', 'C/ Lorenzo, 57, 1ºA', 'Madrid', 'Madrid', '28015', 'petercamo@gmail.com', '688132231', 'Pcmoreno23');
+EXEC Registrar_Voluntario('19358915E', 'Sergio', 'Serrano Rascón', '19/04/1989', 'C/ Carrer Cadena, 14, 21ºA', 'Madrid', 'Madrid', '28034', 'ser2r@gmail.com', '625647435', 'Ssrascon19');
+EXEC Registrar_Voluntario('44919799V', 'Inés', 'Lozano Martínez', '05/05/2000', 'C/ Eras, 34', 'Sevilla', 'Sevilla', '41017', 'inesloma@gmail.com', '647432073', 'Ilmartinez05');
+EXEC Registrar_Voluntario('39891611C', 'José Manuel', 'Abad Ceballos', '29/07/1991', 'Avda. Los llanos, 5, 7ºC', 'Madrid', 'Madrid', '28007', 'jmabad@gmail.com', '668270080', 'JMaceballos29');
+EXEC Registrar_Voluntario('73275330K', 'Miguel', 'Vázquez Noriega', '03/03/1995', 'C/ Los Herrán, 91', 'Sevilla', 'Sevilla', '41015', 'miguelvn@gmail.com', '610713471', 'Mvnoriega03');
+EXEC Registrar_Voluntario('89093705P', 'Valeria', 'Gallego Nieto', '17/04/1898', 'C/ Angosto, 15, 3ºB', 'Barcelona', 'Barcelona', '08012', 'valgani@gmail.com', '688398689', 'Vgnieto17');
+EXEC Registrar_Voluntario('25385282K', 'Blanca', 'Escudero Montoya', '29/09/1994', 'C/ Fernández de Leceta, 27, 2ºA', 'Valencia', 'Valencia', '46019', 'blanca_esc94@gmail.com', '690652694', 'Bemontoya29');
+EXEC Registrar_Voluntario('52204537B', 'Nicolás', 'Griego Corrales', '22/05/1997', 'C/ Cuevas de Ambrosio, 34, 1ºC', 'Madrid', 'Madrid', '28022', 'nicogc97@gmail.com', '618718143', 'Ngcorrales22');
+EXEC Registrar_Voluntario('98369122P', 'Alejandra', 'Romo Calero', '02/07/1992', 'C/ Libertad, 55, 5ºB', 'Madrid', 'Madrid', '28036', 'aleroca@gmail.com', '652632485', 'Arcalero02');
+EXEC Registrar_Voluntario('39661938W', 'Oriol', 'Nava Chacón', '21/09/1993', 'C/ Maestro Puig Valera, 8, 4ºD', 'Barcelona', 'Barcelona', '08023', 'oriol_nc93@gmail.com', '680816635', 'Onchacon21');
+
+EXEC Registrar_Voluntario('74820475A', 'Mireia', 'Jurado Ávalos', '30/06/1996', 'C/ Cañada del Rosal, 29, 1ºA', 'Madrid', 'Madrid', '28011', 'mirejurado96@gmail.com', '617674051', 'Mjavalos31');
+EXEC Registrar_Voluntario('13536072C', 'Alba', 'Pérez Ojeda', '12/10/1989', 'Avda. Alameda Sundheim, 42, 5ºC', 'Sevilla', 'Sevilla', '41008', 'alba.po12@gmail.com', '601444268', 'Apojeda12');
+EXEC Registrar_Voluntario('56344358Q', 'Aitor', 'Lozano Vega', '25/03/1990', 'C/ Benito Guinea, 21, 3ºB', 'Madrid', 'Madrid', '28032', 'ailove90@gmail.com', '684930198', 'Alvega25');
+EXEC Registrar_Voluntario('60356609D', 'Erik', 'Roca García', '03/11/1999', 'Ctra. de Fuentenueva, 34, 2ºD', 'Madrid', 'Madrid', '28044', 'erik_rc99@gmail.com', '606274316', 'Ergarcia03');
+EXEC Registrar_Voluntario('27949209T', 'Clara', 'Perea Villagómez', '05/05/1997', 'C/ Alicante, 11, 6ºA', 'Madrid', 'Madrid', '28029', 'clapevi97@gmail.com', '687807923', 'Cpvillagomez05');
+EXEC Registrar_Voluntario('93645338X', 'Lucas', 'Trejo Armas', '16/07/1995', 'Avda. Andalucía, 17, 6ºC', 'Sevilla', 'Sevilla', '41006', 'lucas_tarmas@gmail.com', '648144356', 'Ltarmas16');
+EXEC Registrar_Voluntario('53914875T', 'Íker', 'Zamora Terra', '24/12/1999', 'Avda. Santiago Lapuente, 27, 5ºB', 'Madrid', 'Madrid', '28043', 'iker.zt99@gmail.com', '732947026', 'Izterra24');
+EXEC Registrar_Voluntario('67118131Z', 'Valentina', 'Cuellar Cárdenas', '27/10/1997', 'Plazuela do Porto, 24', 'Barcelona', 'Barcelona', '08027', 'valentina.cc27@gmail.com', '688601521', 'Vccardenas27');
+EXEC Registrar_Voluntario('45354145P', 'Leyre', 'Páez Heredia', '29/01/1993', 'C/ Herrería, 13, 3ºA', 'Madrid', 'Madrid', '28011', 'lph_29@gmail.com', '689497654', 'Lpheredia29');
+EXEC Registrar_Voluntario('86976758G', 'Martín', 'Sánchez Arias', '11/02/1996', 'C/ Eusebio Dávila, 98, 1ºA', 'Sevilla', 'Sevilla', '41005', 'martin_sanchez96@gmail.com', '636438263', 'Msarias11');
+
+EXEC Registrar_Voluntario('31971064Y', 'Adrián', 'Castillo Flores', '29/01/1990', 'C/ Puerta Nueva, 20, 3ºD', 'Madrid', 'Madrid', '28028', 'adricastillo29@gmail.com', '656731145', 'Acflores29');
+EXEC Registrar_Voluntario('22907649V', 'Miriam', 'Rubio Soler', '21/09/1992', 'Pza. Fuensanta, 60, 1ºB', 'Madrid', 'Madrid', '28015', 'miriam.rs92@gmail.com', '677728533', 'Mrsoler21');
+EXEC Registrar_Voluntario('70905371M', 'Asier', 'Ibáñez Peña', '18/03/1997', 'C/ Cádiz, 42', 'Granada', 'Granada', '18006', 'asier.ip18@gmail.com', '617788511', 'Aipeña18');
+EXEC Registrar_Voluntario('21038062P', 'Miguel Ángel', 'Gutiérrez García', '16/12/1990', 'C/ Pascual Yunquera, 48, 7ºC', 'Barcelona', 'Barcelona', '08014', 'miguelgg16@gmail.com', '659902255', 'MAggarcia16');
+EXEC Registrar_Voluntario('92417527P', 'Javier', 'Sanz Moya', '07/11/2000', 'C/ Rosa de los Vientos, 80', 'Madrid', 'Madrid', '28015', 'javi.sm07@gmail.com', '670969607', 'Jsmoya07');
+EXEC Registrar_Voluntario('52794613E', 'Cristian', 'Méndez Gallardo', '13/01/1998', 'Avda. Enrique Peinador, 73, 5ºA', 'Valencia', 'Valencia', '46004', 'cris_mg98@gmail.com', '627410977', 'Cmgallardo13');
+EXEC Registrar_Voluntario('65249536P', 'Jorge', 'Sanz Fernández', '28/11/1995', 'C/ Socampo, 44, 2ºC', 'Madrid', 'Madrid', '28037', 'jorge.sfer@gmail.com', '628203956', 'Jsfernandez28');
+EXEC Registrar_Voluntario('30787251A', 'Rodrigo', 'Ramos Durán', '09/04/1989', 'Avda. Generalísimo, 72, 4ºD', 'Madrid', 'Madrid', '28051', 'rrd09@gmail.com', '631355343', 'Rrduran09');
+EXEC Registrar_Voluntario('37025606E', 'Eva', 'Costa Benítez', '14/08/1994', 'Avda. Cervantes, 9, 6ºB', 'Granada', 'Granada', '28009', 'evacb94@gmail.com', '664654255', 'Ecbenitez14');
+EXEC Registrar_Voluntario('73639982P', 'Nuria', 'Caballero Sanz', '02/05/1997', 'C/ Lamas Carvajal, 45, 2ºA', 'Madrid', 'Madrid', '28048', 'nuri.cs97@gmail.com', '620295442', 'Ncsanz02');
+
+EXEC Registrar_Voluntario('87485443K', 'Patricia', 'Delgado Vicente', '10/07/1993', 'C/ Hernán Cortés, 55, 4ºC', 'Sevilla', 'Sevilla', '41006', 'patri_dv10@gmail.com', '683756384', 'Pdvicente10');
+EXEC Registrar_Voluntario('49833268L', 'Alonso', 'Grau García', '17/09/1996', 'C/ Canarias, 69, 8ºB', 'Madrid', 'Madrid', '28013', 'alonso2g_96@gmail.com', '629851656', 'Aggarcia17');
+EXEC Registrar_Voluntario('29480606B', 'Aaron', 'Gallardo Prieto', '23/02/1995', 'C/ Ruiseñor, 27, 2ºD', 'Granada', 'Granada', '18007', 'agp.2302@gmail.com', '642847055', 'Agprieto23');
+EXEC Registrar_Voluntario('53632035Z', 'Claudia', 'Durán Martínez', '20/10/2000', 'C/ Álvaro Cunqueiro, 95, 5ºC', 'Barcelona', 'Barcelona', '08033', 'clau_dm00@gmail.com', '720251903', 'Cdmartinez20');
+EXEC Registrar_Voluntario('44065022N', 'Fernando', 'Fernandez Herrero', '15/03/1989', 'C/ Calvo Sotelo, 73, 3ºA', 'Madrid', 'Madrid', '28019', 'ferfer89@gmail.com', '613455531', 'Ffherrero15');
+EXEC Registrar_Voluntario('91564155M', 'Pablo', 'Serra Mora', '25/12/1998', 'C/ Manuel Iradier, 17', 'Madrid', 'Madrid', '28040', 'pablo.sm25@gmail.com', '781660372', 'Psmora25');
+EXEC Registrar_Voluntario('75490429J', 'Óscar', 'Muñoz Morales', '06/08/1900', 'C/ Estrella, 32, 6ºD', 'Granada', 'Granada', '18010', 'oscar_mm00@gmail.com', '652381733', 'Ommorales06');
+EXEC Registrar_Voluntario('18378738J', 'Carlota', 'Molina Rubio', '29/06/1991', 'C/ Inglaterra, 38, 5ºA', 'Madrid', 'Madrid', '28002', 'carlota.mr29@gmail.com', '686826369', 'Cmrubio29');
+EXEC Registrar_Voluntario('32412109A', 'Cristina', 'Reyes Mora', '04/02/1990', 'C/ San Andrés, 14, 4ºB', 'Sevilla', 'Sevilla', '41003', 'cris04rm@gmail.com', '622490388', 'Crmora04');
+EXEC Registrar_Voluntario('74483068Y', 'Esteban', 'Medina Ortiz', '30/01/1994', 'C/ Cuatro Esquinas, 30, 2ºC', 'Madrid', 'Madrid', '28038', 'esteban.medina94@gmail.com', '611519217', 'Emortiz30');
+
