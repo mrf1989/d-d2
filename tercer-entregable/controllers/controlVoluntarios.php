a200ecabbef99aaae2601a21e9289a99a3bd9f06
Elena Molino <44238816+marmolpen3@users.noreply.github.com>
2019-04-19 18:48:16 +0000

    Avances en la rama de voluntarios

--- tercer-entregable/controllers/controlVoluntarios.php
+++ tercer-entregable/controllers/controlVoluntarios.php
@@ -15,4 +15,38 @@ if($_REQUEST["submit"]=="delete"){
     cerrarConexionBD($conexion);
     Header("Location: ../voluntarios.php");
 }
+elseif ($_REQUEST["submit"]=="insert") {
+	$nuevoVoluntario["nombre"] = $_REQUEST["nombre"];
+    $nuevoVoluntario["apellidos"] = $_REQUEST["apellidos"];
+    $nuevoVoluntario["dni"] = $_REQUEST["dni"];
+    $nuevoVoluntario["fechaNacimiento"] = getFechaBD($_REQUEST["fechaNacimiento"]);
+    $nuevoVoluntario["email"] = $_REQUEST["email"];
+    $nuevoVoluntario["telefono"] = $_REQUEST["telefono"];
+    $nuevoVoluntario["direccion"] = $_REQUEST["direccion"];
+    $nuevoVoluntario["localidad"] = $_REQUEST["localidad"];
+    $nuevoVoluntario["provincia"] = $_REQUEST["provincia"];
+    $nuevoVoluntario["cp"] = $_REQUEST["cp"];
+
+    $conexion = crearConexionBD();
+    getInsertarVoluntario($conexion, $nuevoVoluntario);
+    cerrarConexionBD($conexion);
+    Header("Location: ../voluntarios.php");
+}
+elseif ($_REQUEST["submit"]=="edit") {
+	$voluntario["nombre"] = $_REQUEST["nombre"];
+    $voluntario["apellidos"] = $_REQUEST["apellidos"];
+    $voluntario["dni"] = $_REQUEST["dni"];
+    $voluntario["fechaNacimiento"] = getFechaBD($_REQUEST["fechaNacimiento"]);
+    $voluntario["email"] = $_REQUEST["email"];
+    $voluntario["telefono"] = $_REQUEST["telefono"];
+    $voluntario["direccion"] = $_REQUEST["direccion"];
+    $voluntario["localidad"] = $_REQUEST["localidad"];
+    $voluntario["provincia"] = $_REQUEST["provincia"];
+    $voluntario["cp"] = $_REQUEST["cp"];
+
+    $conexion = crearConexionBD();
+    getActualizarVoluntario($conexion, $voluntario);
+    cerrarConexionBD($conexion);
+    Header("Location: ../perfilVoluntario.php?oid_vol=" . $_REQUEST["oid_vol"]);
+}
 ?>
--- tercer-entregable/voluntarios.php
+++ tercer-entregable/voluntarios.php
@@ -3,13 +3,36 @@ session_start();
 
 require_once("models/gestionBD.php");
 require_once("models/gestionVoluntarios.php");
+require_once("models/paginacion.php");
 
 if(!$_SESSION["admin"]){
 	header("Location: index.php");
 }
+if (isset($_SESSION["paginacion"])) $paginacion = $_SESSION["paginacion"];
 
-$conexion=crearConexionBD();
-$voluntarios= getVoluntarios($conexion);
+$pag_selected = isset($_GET["pag_num"]) ? (int) $_GET["pag_num"] :
+    (isset($paginacion) ? (int) $paginacion["pag_num"] : 1);
+$pag_size = isset($_GET["pag_size"]) ? (int) $_GET["pag_size"] :
+    (isset($paginacion) ? (int) $paginacion["pag_size"] : 10);
+
+if ($pag_selected < 1) $pag_selected = 1;
+if ($pag_size < 1) $pag_size = 10;
+
+unset($_SESSION["paginacion"]);
+
+$conexion = crearConexionBD();
+
+$query = getAllVoluntarios();
+$total_registros = sizeConsulta($conexion, $query);
+$total_paginas = (int) ($total_registros / $pag_size);
+if ($total_registros % $pag_size > 0) $total_paginas++; 
+if ($pag_selected > $total_paginas) $pag_selected = 1;
+
+$paginacion["pag_num"] = $pag_selected;
+$paginacion["pag_size"] = $pag_size;
+$_SESSION["paginacion"] = $paginacion;
+
+$voluntarios = consultaPaginada($conexion, $query, $pag_selected, $pag_size);
 cerrarConexionBD($conexion);
 
 $page_title= "Voluntarios";
@@ -25,6 +48,23 @@ include_once("includes/head.php");
                     <h1>Voluntarios</h1>
                     <a href="nuevoVoluntario.php" class="btn primary">Nuevo voluntario</a>
                 </div>
+                <nav>
+                    <div id="enlaces">
+                <?php for ($pagina = 1; $pagina <= $total_paginas; $pagina++) {
+                    if ($pagina == $pag_selected) { ?>
+                        <span class="current"><?php echo $pagina; ?></span>
+                    <?php } else { ?>
+                        <a href="<?php $_SERVER["PHP_SELF"] ?>?pag_num=<?php echo $pagina; ?>&pag_size=<?php echo $pag_size; ?>"><?php echo $pagina; ?></a>
+                    <?php } ?>
+                <?php } ?>
+                    </div>
+                    <form method="get">
+                        <input type="hidden" id="pag_num" name="pag_num" value="<?php echo $pag_selected; ?>" />
+                        Mostrando
+                        <input type="number" id="pag_size" name="pag_size" min="1" max="<?php echo $total_registros; ?>" value="<?php echo $pag_size; ?>" autofocus="autofocus" /> entradas de un total de <?php echo $total_registros ?>
+                        <button type="submit" class="btn secondary">Actualizar</button>
+                    </form>
+                </nav>
                 <table class="tab horizontal">
                     <tr>
                         <th>DNI</th>
@@ -35,7 +75,6 @@ include_once("includes/head.php");
                 <?php foreach ($voluntarios as $vol) { ?>
                     <tr>
                         <form action="controllers/controlVoluntarios.php" method="POST">
-                            <!-- Mandamos este input oculto para obtener el dni del voluntario exacto que queremos para eliminar o modificar sus datos-->
                             <input type="hidden" name="dni" value="<?php echo $vol["DNI"] ?>">
                             <td><?php echo $vol["DNI"] ?></td>
                             <!-- Puesto que el nombre es un enlace al perfil del voluntario, mandamos en su url el oid del voluntario y por lo tanto el método sería GET, ese oid lo utilizamos para conseguir los datos del voluntario exacto -->
