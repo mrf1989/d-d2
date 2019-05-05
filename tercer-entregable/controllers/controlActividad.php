2d45cfaf0625839940a013f1d514de370d6dc2d0
marmolpen3 <marmolpen3@65c6c944-e886-468d-9605-cd19f062a6ae>
2019-05-05 09:17:47 +0000

    Validaciones de actividades, incompleta las fechas

--- tercer-entregable/controllers/controlActividad.php
+++ tercer-entregable/controllers/controlActividad.php
@@ -21,16 +21,22 @@ if ($_REQUEST["submit"] == "insert") {
     $nuevaActividad["oid_proj"] = $_REQUEST["oid_proj"];
     
     $conexion = crearConexionBD();
+    $errores= validarAltaActividad($nuevaActividad);
+    if (count($errores)>0) {
+        $_SESSION["errores"] = $errores;
+        Header("Location: ../formActividad.php?oid_proj=". $nuevaActividad["oid_proj"]);
+    }else{
+        insertarActividad($conexion, $nuevaActividad);
+        cerrarConexionBD($conexion);
+        Header("Location: ../perfilProyecto.php?oid_proj=" . $nuevaActividad["oid_proj"]);
+    }
 
-    insertarActividad($conexion, $nuevaActividad);
-    cerrarConexionBD($conexion);
-    Header("Location: ../perfilProyecto.php?oid_proj=" . $_REQUEST["oid_proj"]);
 } else if ($_REQUEST["submit"] == 'delete') {
     $conexion = crearConexionBD();
-
     eliminarActividad($conexion, $_REQUEST["oid_act"]);
     cerrarConexionBD($conexion);
     Header("Location: ../perfilProyecto.php?oid_proj=" . $_REQUEST["oid_proj"]);
+
 } else if ($_REQUEST["submit"] == 'edit') {
     $actividad["oid_act"] = $_REQUEST["oid_act"];
     $actividad["nombre"] = $_REQUEST["nombre"];
@@ -42,13 +48,19 @@ if ($_REQUEST["submit"] == "insert") {
     $actividad["costetotal"] = $_REQUEST["costetotal"];
 
     $conexion = crearConexionBD();
-    $accionOK = actualizarActividad($conexion, $actividad);
+    $errores= validarAltaActividad($actividad);
+    if (count($errores)>0) {
+        $_SESSION["errores"]=$errores;
+        Header("Location: ../formActividad.php?edit=true&oid_act=" . $_REQUEST["oid_act"]);
+    }else{
+        $accionOK = actualizarActividad($conexion, $actividad);
     cerrarConexionBD($conexion);
     if ($accionOK) {
         Header("Location: ../perfilActividad.php?oid_act=" . $actividad["oid_act"]);
     } else {
         $_SESSION["excepcion"] = "No se ha podido actualizar correctamente.";
         Header("Location: ../excepcion.php");
+        }
     }
 }
 
