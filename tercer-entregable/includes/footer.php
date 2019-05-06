878fa017c59ecf8e9dd763a04dc61fad2cba11c7
marruafer <marruafer@65c6c944-e886-468d-9605-cd19f062a6ae>
2019-05-06 11:04:17 +0000

    Header responsive

--- tercer-entregable/includes/footer.php
+++ tercer-entregable/includes/footer.php
@@ -3,4 +3,5 @@
         <p>&copy; 2019, Deporte y Desafío, IISSI-2</p>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="js/menu.js"></script>
 </footer>
