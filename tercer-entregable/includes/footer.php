32b313c81d399464905bfd96ad1608513280da32
Juan Carlos Cortés Muñoz <56033673+juacormun@users.noreply.github.com>
2019-05-07 19:07:19 +0000

    Añadido al proyecto documentacion.php con modificaciones

--- tercer-entregable/includes/footer.php
+++ tercer-entregable/includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div class="content">
-        <p>&copy; 2019, Deporte y Desafío, IISSI-2</p>
+        <p>&copy; 2019, <a href="../includes/documentacion.php">Deporte y Desafío</a>, IISSI-2</p>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="js/menu.js"></script>
