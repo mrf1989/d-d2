05473465131ff2375c8022e4d8ecce8d6dd42be7
marruafer <marruafer@65c6c944-e886-468d-9605-cd19f062a6ae>
2019-05-07 10:37:52 +0000

    Error en formIntereses.php solucionado. Actualiza correctamente el estado de interés de cada actividad.

--- tercer-entregable/controllers/controlIntereses.php
+++ tercer-entregable/controllers/controlIntereses.php
@@ -8,24 +8,27 @@ if (!isset($_SESSION["user"])) {
 include_once("../models/gestionBD.php");
 include_once("../models/gestionIntereses.php");
 
-if (isset($_GET["estado"])) {
+if (isset($_GET["oid_act"])) {
     $interes["dni"] = $_SESSION["login"];
     $interes["oid_act"] = $_GET["oid_act"];
-    $interes["estado"] = $_GET["estado"];
+    if (isset($_GET["estado"])) {
+        $interes["estado"] = $_GET["estado"];
+    } else {
+        $interes["estado"] = $_GET["estado_hidden"];
+    }
+    
+
     $conexion = crearConexionBD();
     if ($_SESSION["user"] == 2) {
         $resultado = actualizarInteresPart($conexion, $interes);
     } else {
         $resultado = actualizarInteresvOL($conexion, $interes);
     }
-    unset($interes);
     cerrarConexionBD($conexion);
     if ($resultado) {
-        $_SESSION["success"] = "Cambios guardados";
-        unset($resultado);
         Header("Location: ../formIntereses.php");
     } else {
-        $_SESSION["excepcion"] = "No se ha actualizado correctamente el estado de interés en la actividad.";
+        $_SESSION["excepcion"] = "No se ha actualizado correctamente el estado de interés a " . $_GET["estado"] . " en la actividad " . $_GET["oid_act"] ;
         Header("../excepcion.php");
     }
 } else {
